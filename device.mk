262627bf19f6c5dad47d88b081f09616e70590aa
taotaoS2 <50801963+taotaoS2@users.noreply.github.com>
2023-10-30 18:49:35 -0300

    moba: Switch to flattened APEXes 1,5/2

--- device.mk
+++ device.mk
@@ -7,9 +7,6 @@
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # APEX
 PRODUCT_COMPRESSED_APEX := false
 
