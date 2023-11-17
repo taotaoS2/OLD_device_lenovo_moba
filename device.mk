d6be5f2c8071009e585569c308e0ff6f239fc0d2
taotaoS2 <superboydbr@gmail.com>
2023-11-17 06:36:36 -0800

    moba: Try to fix keymaster, and use dump manifest.xml
    Signed-off-by: taotaoS2 <superboydbr@gmail.com>

--- device.mk
+++ device.mk
@@ -17,8 +17,6 @@ PRODUCT_PACKAGES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0.vendor \
-    android.hardware.keymaster@4.0.vendor \
     android.hardware.keymaster@4.1.vendor
 
 # Permissions
