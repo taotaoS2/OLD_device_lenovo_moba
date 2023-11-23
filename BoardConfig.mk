334f8d7b3c030fd77d751b9d1a6514bae43032f6
taotaoS2 <superboydbr@gmail.com>
2023-11-23 12:58:36 -0300

    moba: Add bluetooth
    Signed-off-by: taotaoS2 <superboydbr@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,12 +1,21 @@
 #
-# Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2017-2022 The LineageOS Project
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 DEVICE_PATH := device/lenovo/moba
 
-# Hacks
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 BUILD_BROKEN_ENFORCE_SYSPROP_OWNER := true
@@ -101,6 +110,10 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/prebuilts/dtb.img:$(TARGET_COPY_OUT)/dtb.img \
     $(DEVICE_PATH)/prebuilts/kernel:kernel
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(COMMON_PATH)/bluetooth
+TARGET_USE_QTI_BT_STACK := true
+
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
 
@@ -138,10 +151,6 @@ BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := \
     vendor \
     product \
     odm
-
-# Platform
-TARGET_BOARD_PLATFORM := kona
-BOARD_USES_QCOM_HARDWARE := true
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/props/system.prop
